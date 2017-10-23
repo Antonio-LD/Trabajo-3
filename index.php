cd7e3ee4adaa2d3ada3265fa86d57a42709c2724
Antonio-LD <30883556+Antonio-LD@users.noreply.github.com>
2017-10-23 18:48:47 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<html lang="es">
+ <head>
+  <title>Ejercicis En PHP</title>
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" type="text/css" href="css/Style.css">
+ </head>
+ <body>
+  <div id="encabezado">
+   <h1>VARIABLES</h1>
+  <div id="menu">
+   <ul>
+    <li><a href="ejercicio1.php" title="Ejercicio 1">Ejercicio 1</a></li>
+	<li><a href="ejercicio2.php" title="Ejercicio 2">Ejercicio 2</a></li>
+	<li><a href="ejercicio3.php" title="Ejercicio 3">Ejercicio 3</a></li>
+	<li><a href="ejercicio4.php" title="Ejercicio 4">Ejercicio 4</a></li>
+	<li><a href="ejercicio5.php" title="Ejercicio 5">Ejercicio 5</a></li>
+   </ul>
+  </div>
+  </div>
+  <table WIDTH=100% border="0" ALIGN="center">
+  <tr>
+  <td ALIGN="Center">
+  <img src="img/phpvariables.jpg" align="center" width="360" height="360"><br>
+  </td>
+  </tr>
+  <tr>
+  <td ALIGN="Center">
+  <img src="img/varimg.jpg">
+  </td>
+  </tr>
+  </table>
+ </body>
+</html>
